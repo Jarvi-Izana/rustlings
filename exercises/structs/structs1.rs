c17bc27f2c55df4b41d313d2b4b932f57b35d02b
Guihao-Liang-Mini-0 <izana0910@gmail.com>
2019-09-15 21:27:27 -0700

    okay, review struct

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -4,11 +4,13 @@
 // I AM NOT DONE
 
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    name: String,
+    hex: String,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(String, String);
 
+// struct without any fields
 #[derive(Debug)]
 struct UnitStruct;
 
@@ -18,8 +20,10 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            name: "green".to_string(),
+            hex: "#00FF00".to_string(),
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -27,8 +31,9 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        // TODO: Instantiate a tuple struct!
-        // let green =
+        // Instantiate a tuple struct!
+        // For more fun, use the field initialization shorthand.
+        let green = ("green".to_string(), "#00FF00".to_string());
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -36,8 +41,10 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        // Instantiate a unit struct!
+        let unit_struct = UnitStruct;
+
+        // debug print by trait Debug
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
