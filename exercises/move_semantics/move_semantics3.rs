9dca66247b09c649a5eff12a27327daecf8969c4
Guihao-Liang-Mini-0 <izana0910@gmail.com>
2019-11-28 14:35:07 -0800

    move and macros

--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -3,7 +3,12 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` for hints :)
 
-// I AM NOT DONE
+
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
 fn main() {
     let vec0 = Vec::new();
@@ -15,12 +20,20 @@ fn main() {
     vec1.push(88);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+    print_type(vec1);
+
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
     vec
+}
+
+fn print_type(mut vec: Vec<i32>) {
+    println!("{}", type_of(vec));
 }
--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,19 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($val:expr) => {
+        // solution 1, create a long expression
+        // {
+        //     let mut ret = "Hello ".to_string();
+        //     ret.push_str(&$name);
+        //     ret
+        // }
+
+        // format the output
+        format!("Hello {}", $val)
+    }
+}
 
 fn main() {
     if my_macro!("world!") != "Hello world!" {
